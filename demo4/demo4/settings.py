14f8f0e25b3f767906293e728e8165fc6ebb87a0
shouju <1120648012@qq.com>
2019-05-27 19:31:43 +0800

    sttting  xiugai

--- demo4/demo4/settings.py
+++ demo4/demo4/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7bvki3r$hlv1jqcpr*nw4=wi(_+@*+c=8@sgv_b&sfok8mrxf^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
