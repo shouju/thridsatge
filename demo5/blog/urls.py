8914eeab4b6ab066877795be600f9e9cbade9e84
shouju <1120648012@qq.com>
2019-05-30 10:48:19 +0800

    博客 邮件添加

--- demo5/blog/urls.py
+++ demo5/blog/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     url(r'^indexone/$',views.indexone,name='indexone'),
     url(r'^detail/(\d+)/$',views.detail,name='detail'),
     url(r'^about/$',views.about,name='about'),
-    url(r'^contact/$',views.contact,name='contact'),
+    # url(r'^contact/$',views.contact,name='contact'),
+    url(r'^categories/(\d+)/$',views.categories,name='categories'),
+    url(r'^contact/$',views.Contact.as_view(),name='contact'),
     url(r'^index/$',views.index,name='index'),
 ]
--- demo5/blog/views.py
+++ demo5/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,get_object_or_404,redirect,reverse
 from . models import Categories,Tag,Dynamic
 from django.core.paginator import Paginator
+from django.views.generic import View
+from django.core.mail import send_mail,send_mass_mail
+from django.core.mail import settings
 # Create your views here.
 
 def index(request):
@@ -10,7 +13,7 @@ def index(request):
     else:
         pagenum=pagenum
     dynamics=Dynamic.objects.all().order_by('-views')
-    paginator=Paginator(dynamics,3)
+    paginator=Paginator(dynamics,6)
     page=paginator.get_page(pagenum)
 
     return render(request,'blog/index.html',{'page':page})
@@ -22,7 +25,7 @@ def indexone(request):
     else:
         pagenum = pagenum
     dynamics=Dynamic.objects.all().order_by('-views')
-    paginator = Paginator(dynamics, 3)
+    paginator = Paginator(dynamics, 6)
     page = paginator.get_page(pagenum)
     return render(request, 'blog/homepage-2.html',{'page':page})
 
@@ -33,7 +36,34 @@ def detail(request,id):
 def about(request):
     return render(request,'blog/about.html')
 
-def contact(request):
-    return render(request, 'blog/contact.html')
+class Contact(View):
+    def get(self,request):
+        return render(request, 'blog/contact.html',locals())
+    def post(self,request):
+        send_mail("测试邮件", "dashduahduasdjaisdhauihd", settings.DEFAULT_FROM_EMAIL, ['1120648012@qq.com', "275445831@qq.com"])
+        msg=''
+        return render(request,'blog/contact.html',{'msg':'发送成功'})
+
+
+def categories(request,id):
+    dynamics=get_object_or_404(Categories,pk=id).dynamic_set.all()
+    paginator = Paginator(dynamics, 6)
+    page = paginator.get_page(6)
+    return render(request, 'blog/homepage-2.html', {'page': page})
+
+
+# pagenum = request.GET.get('page')
+    # if pagenum == None:
+    #     pagenum = 1
+    # else:
+    #     pagenum = pagenum
+    # dynamics=get_object_or_404(Categories,pk=id).dynamic_set.all()
+    # paginator = Paginator(dynamics, 6)
+    # page = paginator.get_page(pagenum)
+    # return render(request, 'blog/homepage-2.html', {'page': page})
+
+
+
+
 
 
